9cb5e0387b0f0d6f589c8ec8b9248599fd860207
edd-ie <ogendoeddie@gmail.com>
2019-11-19 10:34:12 +0300

    documentation

--- assignment.py
+++ assignment.py
@@ -3,20 +3,20 @@ string2 = "Doe"
 string3 = string1+string2
 print(string3)
 
-z = len(string3)
+z = len(string3)  # calculation of string length
 print(z)
 
-e = string3.count("o")
+e = string3.count("o")  # number of specified character
 print(e)
 
 g = "We are learning how to program in python. I find python programming fun"
-freq = g.count("python")
+freq = g.count("python")  # frequency of appearance of a particular value
 print(freq)
 
 x = "reven"
-rev = x[::-1]
+rev = x[::-1]  # reverse output of a specified value
 print(rev)
 
 p = "racecar"
-txt = p[::-1]
+txt = p[::-1]  # reverse output of a specified value
 print(txt)
