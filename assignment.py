6ba13016aea2fea59f1d47b9e5481bd95889c53c
edd-ie <ogendoeddie@gmail.com>
2019-11-19 10:27:07 +0300

    initial assgn

---
+++ assignment.py
@@ -0,0 +1,22 @@
+string1 = "John"
+string2 = "Doe"
+string3 = string1+string2
+print(string3)
+
+z = len(string3)
+print(z)
+
+e = string3.count("o")
+print(e)
+
+g = "We are learning how to program in python. I find python programming fun"
+freq = g.count("python")
+print(freq)
+
+x = "reven"
+rev = x[::-1]
+print(rev)
+
+p = "racecar"
+txt = p[::-1]
+print(txt)
