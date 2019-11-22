29b02f2f46b1333c9e095bf9712cc67c361faea6
edd-ie <ogendoeddie@gmail.com>
2019-11-22 21:07:56 +0300

    functions

--- assign 4.py
+++ assign 4.py
@@ -21,3 +21,36 @@ for n in range(2, 10):
     for x in range(2, n):
         if n % x == 0:
             print(n, 'equals', x, '*', n//x)
+
+# generate list of numbers x = [1 ,2, 3, 4, 5]
+# sort even & odd then print
+# simulate a sim pin input if pin is wrong 3 tries deny access
+
+x = range(0, 11)
+for num in x:
+    if num % 2 == 0:
+        print(num, "Even")
+    else:
+        print(num, "Odd")
+
+
+pin = 1234
+x = 0
+while x < 3:
+    secure = int(input("Enter Pin: "))
+    if secure == pin:
+        print("Device Unlocked!")
+        break
+    else:
+        print("Wrong Pin!")
+    x += 1
+
+
+
+
+
+
+
+
+
+ 
---
+++ function.py
@@ -0,0 +1,70 @@
+"""
+Group  of related statement....
+1  Keyword def marks the start of function header.
+                    def - function - name (parameters):
+                      	statement(s)
+
+
+"""
+p = 0
+while p < 10:
+    p = p + 1
+    print(p, "Hello world")
+
+
+# defining & creating function
+def x_e():
+    # function body
+    print("hello")
+    print("I'm ok")
+
+
+# call the function
+x_e()
+
+q = list(range(1, 7))
+r = list(range(10, 19))
+s = list(range(20, 30))
+
+
+def first_in_list(var):
+    print(var[-1])
+
+
+first_in_list(s)
+
+
+def my_name(name):
+    print(name)
+
+
+my_name("Eddie")
+
+
+def add_2(a, b):
+    ph = a + b
+    print(ph)
+
+
+add_2(10, 20)
+
+
+def sub_2(a, b):
+    diff = a - b
+    return diff
+
+
+sub_2(23, 20)
+
+z = sub_2(10, 5)
+print(z)
+
+
+def name_open(name):
+    return "Hello {} !".format(name)
+
+
+p = name_open("gerald")
+print(p)
+
+# do part A using the grading sys i created, B {don't do milestone tasks}
